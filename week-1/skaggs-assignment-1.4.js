5e601e73658a0124276b590746d6323a53bfa24a
jskaggs80 <jskaggs80@yahoo.com>
2018-05-24 20:50:37 -0500

    Adding skaggs-assignment-1.4.js

---
+++ week-1/skaggs-assignment-1.4.js
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 24 May 2018
+; Modified By: Jake Skaggs
+; Description: Primitive Types
+;===========================================
+*/ 
+
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Jake";
+var lastName = "Skaggs";
+var age = "37";
+var street = "123 Main St.";
+var city = "Omaha";
+var state = "NE";
+var zipCode = "68137";
+
+
+// output 
+console.log(firstName+ ' ' + lastName + '\n' + street + '\n' + city + ', ' + state + ' ' + zipCode); 
+
+
+
+// end program 
