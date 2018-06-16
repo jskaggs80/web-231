960b0ef1ca5e5fd3f1bcbe8d695f6d2569d835a5
jskaggs80 <jskaggs80@yahoo.com>
2018-06-16 11:50:29 -0500

    Adding Assignment-4.4.js

---
+++ week-4/skaggs-assignment-4.4.js
@@ -0,0 +1,70 @@
+/*
+============================================
+; Title:  skaggs-assignment-4.4.js
+; Author: Professor Krasso 
+; Date:   16 June 2018
+; Modified By: Jake Skaggs
+; Description: Arrays
+;===========================================
+*/
+
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Assignment 4.4
+ Today's Date
+
+ -- ORIGINAL ARRAY --
+ Alabama
+ Nebraska
+ Iowa
+ California
+ Nevada
+
+ -- SORTED ARRAY --
+ Alabama
+ California
+ Iowa
+ Nebraska
+ Nevada
+
+ -- SELECTED VALUE --
+ Iowa
+
+ */
+
+// start program
+
+var states = ["Alabama", "Nebraska", "Iowa", "California", "Nevada"]
+
+// function
+function getValue(x, val) {
+    for (var i = 0; i < x.length; i++){
+        if (x[i] === val) {
+            console.log(x[i]);
+        }
+    }
+}
+
+
+// Output
+console.log(header.display("Jake", "Skaggs", "Exercise 4.4") + "\n");
+console.log("-- Original Array --");
+//for loop to itterate through and print states
+for (var j = 0; j < states.length; j++) {
+    console.log(states[j]);
+}
+//sorted Array
+console.log("\n" + "-- SORTED ARRAY --")
+states.sort();
+var s = states.join(" " + "\n");
+console.log(s);
+//Selected Value
+console.log("\n" + "-- SELECTED VALUE --")
+getValue(states, "Nebraska");
+
+
+// end program
