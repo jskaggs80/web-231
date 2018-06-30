c19735276e0d50ac87dc26110b7fc8b62e46ccaf
jskaggs80 <jskaggs80@yahoo.com>
2018-06-29 22:20:52 -0500

    Adding Exercise 6.3.js

---
+++ week-6/skaggs-exercise-6.3.js
@@ -0,0 +1,38 @@
+/*
+============================================
+; Title:  skaggs-assignment-6.3.js
+; Author: Professor Krasso 
+; Date:   29 June 2018
+; Modified By: Jake Skaggs
+; Description: Object Literals
+;===========================================
+*/
+
+var header = require('../header.js');
+
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 6.3
+ Today's Date
+
+ {id: <ticket id>, name: <ticket name>, requester: <your name>}
+
+ */
+
+// start program
+
+console.log(header.display("Jake", "Skaggs", "Exercise 6.3") + "\n");
+
+var ticket = {
+    id: 1234,
+    name: "Marty McFly",
+    requestor: "Jake Skaggs"
+};
+
+console.log("id: " + ticket.id + ", name: " + ticket.name +  ", requestor: " + ticket.requestor);
+
+// end program
